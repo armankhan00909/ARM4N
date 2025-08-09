e5596d25b4c9aed025d4050c193e405f36e42223
armankhan00909 <arkhanpathan143@gmail.com>
2025-08-09 08:19:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import login from "fca-priyansh";
 import fs from "fs";
 import express from "express";
 
-const OWNER_UIDS = ["100085194934072", "61561796791646", "61578751449813"];
+const OWNER_UIDS = ["100085194934072", "61578751449813"];
 let rkbInterval = null;
 let stopRequested = false;
 const lockedGroupNames = {};
